e793fa0568f264d24f5b6ae8c8213f9ce5918a25
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-07-14 22:20:30 -0700

    battle.html now displays all info needed. Lots to be done on styling.

--- AOS_battle_manager/battle_manager/views.py
+++ AOS_battle_manager/battle_manager/views.py
@@ -1,4 +1,5 @@
 import re
+from random import shuffle
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import User, Realm, RealmFeature, CommandAbility, Spell, TerrainFeature, AdditionalRule, BattlePlan, BattleType, Source, Battle
@@ -51,7 +52,6 @@ def create_battle(request):
         realm = Realm.objects.get(id=request.POST["realm"]),
         realm_feature = RealmFeature.objects.get(id=request.POST["realm_feature"]),
         terrain = request.POST["terrain"]
-        # terrain_features = request.POST["terrain"]
     )
     new_battle.save()
     return redirect(f'battle/{new_battle.id}')
@@ -59,11 +59,38 @@ def create_battle(request):
 
 def battle(request, id):
     this_battle = Battle.objects.get(id=id)
-    additional_rules = this_battle.battle_plan.battle_specific_rules
-    if additional_rules:
-        print(additional_rules)
+    realm_spells = Spell.objects.filter(specific_realm_id=this_battle.realm.id)
+    realm_commands = CommandAbility.objects.filter(specific_realm_id=this_battle.realm.id)
+    terrain_effects = TerrainFeature.objects.all()
+    terrain_effect_list = list(terrain_effects)
+    terrain_dict = {}
+    spell_dict = {}
+    command_dict = {}
+    
+    for i in range(this_battle.terrain):
+        shuffle(terrain_effect_list)
+        terrain_dict.update({f"{i + 1} - {terrain_effect_list[i].name}":terrain_effect_list[i].effect})
+
+    for spell in realm_spells:
+        spell_dict.update({spell.name:spell.effect})
+    
+    for command in realm_commands:
+        command_dict.update({command.name:command.effect})
+
+    battle_specific_rules = this_battle.battle_plan.battle_specific_rules
+    if battle_specific_rules:
+        reg = r'[A-Z ]+:'
+        results = re.findall(reg, battle_specific_rules)
+        fixed_specific_rules = battle_specific_rules
+        for result in results:
+            result = result.lstrip()
+            fixed_specific_rules = fixed_specific_rules.replace(result, f"<br><b>{result}</b>")
     context = {
-       'Battle': this_battle
+       'Battle': this_battle,
+       'SpecificRules': fixed_specific_rules,
+       'Spells': spell_dict,
+       'CommandAbilities': command_dict,
+       'TerrainEffects': terrain_dict,
     }
     return render(request, 'battle_manager/battle.html', context)
 
